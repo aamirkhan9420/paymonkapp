ffaf25708f28c88bb86b21de34cf877729a6c550
aamirkhan9420 <aamirkhan.z9373@gmail.com>
2023-04-13 12:14:10 +0530

    navbar and Allroutes components added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Navbar from './component/Navbar/Navbar';
+import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-   
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }
